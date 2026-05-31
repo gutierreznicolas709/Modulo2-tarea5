6fc3cc0687d5283143f6420c5bd80a95938e3ee6
Nicolas Gutierrez <ngutierrezj@unicartagena.edu.co>
2026-05-31 15:53:58 -0500

    Se agregaron los archivos para trabajar el server y se creo la carpeta priuebas.txt

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const Puerto = 3000
+
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Servidor activo');
+});
+
+app.get('/saludo', (req, res) => {
+  res.send('Hola comunidad');
+});
+
+app.get('/mensaje/:nombre', (req, res) => {
+  const nombre = req.params.nombre;
+  res.send('Hola ' + nombre);
+});
+
+app.listen(Puerto, () => {
+  console.log('Servidor ejecutándose en puerto' + Puerto);
+  console.log("Puedes abrir http://localhost:" + Puerto+ " en el navegador.")
+});
